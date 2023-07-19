39917b73f8b219b65a2a7d124927a40043b55d12
jongheonleee <qwefghnm1212@gmail.com>
2023-07-19 13:28:38 +0900

    ~handling events

---
+++ .history/src/components/ListGroup_20230719125636.tsx
@@ -0,0 +1,22 @@
+function ListGroup() {
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        items.map(item => <li>{item}</li>)
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
